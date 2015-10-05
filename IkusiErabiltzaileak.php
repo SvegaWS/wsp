3fd06062cf84910dcbf484bda546409f9abc9913
Sergio Vega <svega93bil@gmail.com>
2015-10-05 17:29:35 +0200

    Datu baserako PHP aukeraketa hautazkoa egina bat egina

--- IkusiErabiltzaileak.php
+++ IkusiErabiltzaileak.php
@@ -1,6 +1,9 @@
 <?php
-	mysql_connect("mysql.hostinger.es","u615503288_sws","enekosergio") or die(mysql_error());
-	mysql_select_db("u615503288_erab") or die(mysql_error());
+	//mysql_connect("mysql.hostinger.es","u615503288_sws","enekosergio") or die(mysql_error());
+	//mysql_select_db("u615503288_erab") or die(mysql_error());
+	
+	mysql_connect("localhost","root","") or die(mysql_error());
+	mysql_select_db("quiz") or die(mysql_error());
 	
 	$erabiltzaileak =mysql_query( "select * from erabiltzaileak" );
 		
